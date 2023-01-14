5f55807db57a054664823f2108e9d9a6de125c2a
solomonp <solodax46@gamil.com>
2023-01-14 20:17:45 +0530

    gitone

---
+++ soloprojects/src/Solodax.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+  function Solodax(props) {
+    return (
+        <div>
+            <h1>dhfvkjh {props.name}</h1>
+            </div>
+    )
+    
+}
+
+export default Solodax;
