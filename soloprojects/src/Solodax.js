6accc32fd9764f222a329337c2afc1456fa3d8c9
solomonp <solodax46@gamil.com>
2023-01-24 14:36:48 +0530

    gitnewchanges

--- soloprojects/src/Solodax.js
+++ soloprojects/src/Solodax.js
@@ -3,7 +3,8 @@ import React from 'react'
   function Solodax(props) {
     return (
         <div>
-            <h1>dhfvkjh {props.name}</h1>
+        <h1>dhfvkjh {props.name}</h1>
+        <h1>newsettilement</h1>
             </div>
     )
     
